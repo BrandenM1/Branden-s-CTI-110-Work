c34796ceb3e311977720674feb1af6b4fafad57e
BrandenM1 <mcleanb5997@student.faytechcc.edu>
2026-09-12 18:07:30 -0400

    Add files via upload

---
+++ P1Lab1_McleanBranden.py
@@ -0,0 +1,17 @@
+#Branden McLean
+#September 10, 2026
+#Inputs and Outputs
+
+# Get first name
+First_name = input("Type first name: ")
+
+# Get Last name
+Last_name = input("Type last name: ")
+
+#Output welcome message to user
+print("Hello,", First_name , Last_name + "!!! Welcome to CTI 110" )
+print()
+print()
+print()
+# Use f-string 
+print(f"Hello, {First_name} {Last_name}!!! Welcome to CTI-110")
